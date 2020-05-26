dd5e47d2762fcf6398cde332ff9d43b8d606a0d7
jpamela_root <59771746+jpamela-root@users.noreply.github.com>
2020-05-26 14:42:56 -0300

    Add files via upload

---
+++ post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+<body>
+
+<?php
+$usuario = $_POST["nome"] ;
+$tempo = $_POST["idade"] ;
+
+ echo " Oi.."  .  $usuario  .  "</br>";
+ echo "Voce tem"  .  $tempo   .   "anos";
+
+?>
+</body>
+</html>
